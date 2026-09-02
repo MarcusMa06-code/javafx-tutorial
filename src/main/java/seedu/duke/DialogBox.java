9fa11222546f8f25ce7a9fc155c4256eccc328da
Ma Mingyang <mingyang.ma@u.nus.edu>
2026-09-02 15:57:55 +0800

    Differentiate Duke dialogs
    User and Duke messages currently share the same visual orientation,\nwhich makes speakers difficult to distinguish.\n\nProvide speaker-specific dialog factories so Duke replies appear on the\nleft while user messages retain the original alignment.

--- src/main/java/seedu/duke/DialogBox.java
+++ src/main/java/seedu/duke/DialogBox.java
@@ -1,6 +1,11 @@
 package seedu.duke;
 
+import java.util.Collections;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -27,5 +32,39 @@ public class DialogBox extends HBox {
         setAlignment(Pos.TOP_RIGHT);
 
         getChildren().addAll(messageLabel, displayPicture);
+    }
+
+    /**
+     * Repositions the avatar to the left of the message.
+     */
+    private void flip() {
+        ObservableList<Node> children = FXCollections.observableArrayList(getChildren());
+        Collections.reverse(children);
+        getChildren().setAll(children);
+        setAlignment(Pos.TOP_LEFT);
+    }
+
+    /**
+     * Creates a right-aligned dialog box for a user message.
+     *
+     * @param text message to display.
+     * @param image avatar to display alongside the message.
+     * @return a dialog box for the user.
+     */
+    public static DialogBox getUserDialog(String text, Image image) {
+        return new DialogBox(text, image);
+    }
+
+    /**
+     * Creates a left-aligned dialog box for a Duke message.
+     *
+     * @param text message to display.
+     * @param image avatar to display alongside the message.
+     * @return a dialog box for Duke.
+     */
+    public static DialogBox getDukeDialog(String text, Image image) {
+        DialogBox dialogBox = new DialogBox(text, image);
+        dialogBox.flip();
+        return dialogBox;
     }
 }
